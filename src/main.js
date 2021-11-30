4c5ddba8916e13450b666c64e577262994795710
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-11-30 18:24:24 +0300

    поправил логику отрисовки и последовательность

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
 import {renderTemplate, RenderPosition} from './render.js';
+
 import {createTripMainTripInfo} from './view/trip_main/info/site-trip-main-trip-info-view.js';
 import {createTripMainNavigationTabs} from './view/trip_main/controls/navigation_tabs/site-trip-main-trip-tabs-view.js';
 import {createTripMainControlsTripFilters} from './view/trip_main/controls/trip-controls__filters/site-trip-main-controls-trip-filters-view.js';
 import {createTripMainControlsTripFiltersTabs} from './view/trip_main/controls/trip-controls__filters/tabs/site-trip-main-trip-filters-view.js';
-//page main
-import {createPageMainStatistics} from './view/page_main/statistics/site-page-main-statics-view';
-import {createPageMainStatisticsItem} from './view/page_main/statistics/item/site-page-main-statics-item-view.js';
-//msg
-import {createSitePageEventsMsgLoading} from './view/page_main/events/msg/site-page-main-trip-events-msg-loading-view.js';
-import {createSitePageMainEventsMsgPast} from './view/page_main/events/msg/site-page-main-trip-events-msg -past-view.js';
-import {createSitePageMainEventsMsgFuture} from './view/page_main/events/msg/site-page-main-trip-events-msg -future-view.js';
-import {createSitePageMainEventsMsgEverthing} from './view/page_main/events/msg/site-page-main-trip-events-msg -everthing-view.js';
 
-import {createSitePageMainEventsSort} from './view/page_main/events/sort/site-page-main-trip-events-trip-sort-view.js';
-import {createSitePageMainTripEventsList} from './view/page_main/events/list/site-page-main-trip-events-list-view';
-import {createSitePageMainTripEventsListItem} from './view/page_main/events/list/item/site-page-main-item-view.js';
-
-//forma event
-import {createSitePageMainTripEventsListEvent} from './view/page_main/events/list/event/site-page-main-trip-events-list-event-view.js';
-import {createSitePageMainTripEventsListEventHeader} from './view/page_main/events/list/event/header/site-page-main-trip-event-list-event-header-view.js';
-import {} from './view/page_main/events';
 //ишем контейнер для  создания шапки
-{
 const siteTripMain = document.querySelector('.trip-main');
 
 //ищем куда вставить блок trip-info
@@ -41,25 +25,45 @@ renderTemplate(tripMainTripControlsFilters, createTripMainControlsTripFilters(),
 //добовляем кнопки 3шт, но только после filtres
 const tripMainControlsTripFiltersTabs = tripMainTripControlsFilters.querySelector('.trip-filters');
 renderTemplate(tripMainControlsTripFiltersTabs, createTripMainControlsTripFiltersTabs(), RenderPosition.AFTERBEGIN);
-}
+
+
+//page main
+import {createPageMainStatistics} from './view/page_main/statistics/site-page-main-statics-view';
+import {createPageMainStatisticsItem} from './view/page_main/statistics/item/site-page-main-statics-item-view.js';
+//msg
+import {createSitePageEventsMsgLoading} from './view/page_main/events/msg/site-page-main-trip-events-msg-loading-view.js';
+import {createSitePageMainEventsMsgPast} from './view/page_main/events/msg/site-page-main-trip-events-msg -past-view.js';
+import {createSitePageMainEventsMsgFuture} from './view/page_main/events/msg/site-page-main-trip-events-msg -future-view.js';
+
+
+import {createSitePageMainEventsSort} from './view/page_main/events/sort/site-page-main-trip-events-trip-sort-view.js';
+import {createSitePageMainTripEventsList} from './view/page_main/events/list/site-page-main-trip-events-list-view';
+import {createSitePageMainTripEventsListItem} from './view/page_main/events/list/item/site-page-main-item-view.js';
+
+//forma event
+import {createSitePageMainTripEventsListEvent} from './view/page_main/events/list/event/site-page-main-trip-events-list-event-view.js';
+import {createSitePageMainTripEventsListEventDestination} from './view/page_main/events/list/event/details/destination/site-page-main-events-list-destination-view.js';
+import {createSitePageMainTripEventsListEventOffers} from './view/page_main/events/list/event/details/offers/site-page-main-events-list-offers-view.js';
+
 //ищем контейнер page-Main
 const sitepageMain = document.querySelector('.page-main');
 const pageMainTripEvents = sitepageMain.querySelector('.trip-events');
+//тут настройки сайта
+
+const statistics = false; //true false
+const MSG = 4;//1-4
+const open = 3;//1-9
+
 //тут у нас 2 состояния statistics или events
-const statistics = true;
-const MSG = 1;
-const open = 1;
-const offers = 1;
-const destination = 1;
-//
 if (statistics) {
-  //вставляем статистику
+  //вставляем статистику койнтейнер
   pageMainTripEvents.classList.add('trip-events--hidden');
   renderTemplate(pageMainTripEvents, createPageMainStatistics(), RenderPosition.AFTEREND);
   const pageMainStatistics = sitepageMain.querySelector('.statistics');
-    for(let i = 0; i<3; i++){
-      renderTemplate(pageMainStatistics, createPageMainStatisticsItem(), RenderPosition.AFTEREND);
-    }
+  //вставляем данные статистики
+  for(let i = 0; i<3; i++){
+    renderTemplate(pageMainStatistics, createPageMainStatisticsItem(), RenderPosition.BEFOREEND);
+  }
 } else {
   //вставляем MSG или список и контент
   switch (MSG) {
@@ -73,26 +77,22 @@ if (statistics) {
       renderTemplate(pageMainTripEvents, createSitePageMainEventsMsgFuture(), RenderPosition.BEFOREEND);
       break;
     case 4:
-      renderTemplate(pageMainTripEvents, createSitePageMainEventsMsgEverthing(), RenderPosition.BEFOREEND);
-      break;
-    default:
+      //отрисовка блока сортировки trip-sort данныех списка
       renderTemplate(pageMainTripEvents, createSitePageMainEventsSort(), RenderPosition.BEFOREEND);
+      //отрисовки блока предложение trip-events__list
       renderTemplate(pageMainTripEvents, createSitePageMainTripEventsList(), RenderPosition.BEFOREEND);
-
-      const pageMainTripEventsList = sitepageMain.querySelector('.trip-events__list');
-
+      //поиск блока trip-events__list для вывода данных trip-events__item
+      const pageMainTripEventsList = pageMainTripEvents.querySelector('.trip-events__list');
+      //генерация 9 данных по маршруту trip-events__item
       for(let i=0; i<9; i++){
-        if(open === i){
-          i++;
+        //условие отрисовки формы event
+        if(i===open){
           renderTemplate(pageMainTripEventsList, createSitePageMainTripEventsListEvent(), RenderPosition.BEFOREEND);
-          const pageMainTripEventsListEvent = sitepageMain.querySelector('.event');
-          renderTemplate(pageMainTripEventsListEvent, createSitePageMainTripEventsListEventHeader(), RenderPosition.AFTERBEGIN);
-          const pageMainTripEventsListEvent
-          if(offers === 1){
-            renderTemplate(pageMainTripEventsListEvent, createSitePageMainTripEventsListEventHeader(), RenderPosition.AFTERBEGIN);
-          }
-
+          const pageMainEventEdit = pageMainTripEventsList.querySelector('.event__details');
+          renderTemplate(pageMainEventEdit, createSitePageMainTripEventsListEventOffers(), RenderPosition.AFTERBEGIN);
+          renderTemplate(pageMainEventEdit, createSitePageMainTripEventsListEventDestination(), RenderPosition.BEFOREEND);
         }
+        //стандартный ввывод данных trip-events__item
         renderTemplate(pageMainTripEventsList, createSitePageMainTripEventsListItem(), RenderPosition.BEFOREEND);
       }
 
@@ -100,4 +100,3 @@ if (statistics) {
       break;
   }
 }
-
--- src/view/trip_main/info/site-trip-main-trip-info-view.js
+++ src/view/trip_main/info/site-trip-main-trip-info-view.js
@@ -2,12 +2,12 @@ export const createTripMainTripInfo = () =>
   //Switch trip view
   `
   <section class="trip-main__trip-info  trip-info">
-  //маршрут
+  <!--маршрут-->
             <div class="trip-info__main">
               <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
               <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
             </div>
-//цена
+<!--цена-->
             <p class="trip-info__cost">
               Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
             </p>
