fb0d84617fb205664755803a76c1dd2fbf37cfab
Andrey Kosov < emailAndrejcosov@yandex.ru>
2022-01-06 16:31:19 +0300

    render

--- src/render.js
+++ src/render.js
@@ -1,4 +1,4 @@
-export const RenderPosition = {
+const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
@@ -9,3 +9,27 @@ export const renderTemplate =(container, template, plase) => {
   container.insertAdjacentHTML(plase, template);
 };
 
+const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  return newElement.firstElementChild;
+};
+
+export { RenderPosition, render, createElement };
